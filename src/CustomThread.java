7bc31eae8e77401f242f2afc3022e618afaa62e3
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-11-25 16:30:17 +0530

    creating custom thread by passing runnable interface as Thread class constructor's parameter

--- src/CustomThread.java
+++ src/CustomThread.java
@@ -1,7 +1,7 @@
 public class CustomThread extends Thread{
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(" 1 ");
+            System.out.print(" 1 ");
             try{
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
+        // Custom Thread 1
         CustomThread customThread = new CustomThread();
         customThread.start();
+
+        // Custom Thread 2
+        Runnable myRunnable = () -> {
+            for(int i = 0; i < 8; i++) {
+                System.out.print(" 2 ");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread myThread = new Thread(myRunnable);
+        myThread.start();
+
+        // Main Thread
+        for(int i = 0; i < 3; i++) {
+            System.out.print(" 0 ");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
