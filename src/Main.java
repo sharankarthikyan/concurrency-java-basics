dd734677391f73ebce410be29127ffe5ceef47bd
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-11-26 08:19:32 +0530

    thread join and interrupt

--- src/Main.java
+++ src/Main.java
@@ -11,41 +11,63 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        Thread installThread = new Thread(() -> {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    Thread.sleep(250);
+                    System.out.println("Installation Step " + (i + 1) + " is completed.");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "InstallThread");
+
         Thread thread = new Thread(() -> {
             String threadName = Thread.currentThread().getName();
             System.out.println(threadName + " should take 10 dots to run.");
             for (int i = 0; i < 10; i++) {
                 System.out.print(". ");
                 try {
-                    System.out.println("A. State = " + Thread.currentThread().getState());
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     System.out.println("\nWhoops!! " + threadName + " interrupted.");
-                    System.out.println("A1. State = " + Thread.currentThread().getState());
+                    Thread.currentThread().interrupt(); // We need to call
+                    // interrupt our thread self to work some methods like (isInterrupted())
                     return;
                 }
             }
             System.out.println("\n" + threadName + " completed.");
         });
 
+        long now = System.currentTimeMillis();
+        Thread threadMonitor = new Thread(() -> {
+            while (thread.isAlive()) {
+                try {
+                    Thread.sleep(1000);
+                    if (System.currentTimeMillis() - now > 2000) {
+                        thread.interrupt();
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
+        });
+
         System.out.println(thread.getName() + " is starting.");
         thread.start();
+        threadMonitor.start();
 
-        long now = System.currentTimeMillis();
-        while (thread.isAlive()) {
-            System.out.println("\n Waiting for thread to complete");
-            try {
-                Thread.sleep(1000);
-                System.out.println("B. State = " + thread.getState());
-
-                if (System.currentTimeMillis() - now > 2000) {
-                    thread.interrupt();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
 
-        System.out.println("C. State = " + thread.getState());
+        if (!thread.isInterrupted()) {
+            installThread.start();
+        } else {
+            System.out.println("Previous thread was interrupted, " + installThread.getName() + " can't run.");
+        }
     }
 }
