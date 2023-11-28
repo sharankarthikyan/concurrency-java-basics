7632625e66a745a6cec5d4f26ba6d9a2e7563ef3
sharan.ka <sharan.ka@zohocorp.com>
2023-11-28 18:19:08 +0530

    run this application multiple times you will get wrong response due to memory cache of each thread

---
+++ src/BankAccount.java
@@ -0,0 +1,40 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit (double amount) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        double originalBalance = balance;
+        balance += amount;
+        System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f) : NEW BALANCE: %.0f%n", originalBalance, amount, balance);
+    }
+
+    public void withdraw (double amount) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        double originalBalance = balance;
+        if(amount <= balance) {
+            balance -= amount;
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f) : NEW BALANCE: %.0f%n", originalBalance, amount, balance);
+        } else {
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f) : INSUFFICIENT FUNDS", originalBalance, amount);
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-import java.util.concurrent.TimeUnit;
-
 public class Main {
     public static void main(String[] args) {
+        BankAccount sharanAccount = new BankAccount(10000);
 
+        Thread thread1 = new Thread(() -> sharanAccount.withdraw(2500));
+        Thread thread2 = new Thread(() -> sharanAccount.deposit(5000));
+        Thread thread3 = new Thread(() -> sharanAccount.withdraw(2500));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            // When we invoke the join() method on a thread,
+            // the calling thread (HERE Main thread) goes into a waiting state.
+            // It remains in a waiting state until the referenced thread terminates.
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Balance: " + sharanAccount.getBalance());
     }
 }
