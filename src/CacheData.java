c35be93e878b647180bdc79ee13f95e55a973a9e
sharan.ka <sharan.ka@zohocorp.com>
2023-11-28 12:45:28 +0530

    reader thread stucks - it's due to memory inconsistency - The operating system may read from heap variables, and make a copy of the value, in each thread's own storage cache

---
+++ src/CacheData.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.TimeUnit;
+
+public class CacheData {
+
+    private boolean flag = false;
+
+    public void toggleFlag () {
+        flag = !flag;
+    }
+
+    public boolean isReady() {
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        CacheData cacheData = new CacheData();
+
+        Thread writerThread = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cacheData.toggleFlag();
+            System.out.println("A. Flag set to " + cacheData.isReady());
+        });
+
+        Thread readerThread = new Thread(() -> {
+            while(!cacheData.isReady()) {
+
+            }
+            System.out.println("B. Flag is " + cacheData.isReady());
+        });
+
+        writerThread.start();
+        readerThread.start();
+    }
+}
