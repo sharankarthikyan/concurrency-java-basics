842f891d258e3e93083227a1bdea10b3a97b3065
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-12-02 11:34:44 +0530

    created threadfactory and running countdown using executorservice with threadcolor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+class ColorThreadFactory implements ThreadFactory {
+
+    private String threadName;
+
+    public ColorThreadFactory(ThreadColor color) {
+        this.threadName = color.name();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r);
+        thread.setName(threadName);
+        return thread;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        ExecutorService blueExecutor = Executors.newSingleThreadExecutor();
+        ExecutorService blueExecutor = Executors.newSingleThreadExecutor(new ColorThreadFactory(ThreadColor.ANSI_BLUE));
         blueExecutor.execute(Main::countDown);
         blueExecutor.shutdown();
     }
